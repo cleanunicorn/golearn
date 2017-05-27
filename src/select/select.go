cd2f6e01d96483dd4bc53d3fb302aef5975dbee8
Daniel Luca <daniel@elasticorange.com>
2017-05-27 23:26:14 +0300

    Testing select

---
+++ src/select/select.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	var c1 = make(chan string)
+	var c2 = make(chan string)
+
+	go func() {
+		for {
+			c1 <- "from 1"
+			time.Sleep(time.Second * 3)
+		}
+	}()
+
+	go func() {
+		for {
+			c2 <- "from 2"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+
+	go func() {
+		i := 0
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println("[1]", msg1, i)
+				i = 0
+			case msg2 := <-c2:
+				fmt.Println("[2]", msg2, i)
+				i = 0
+			case <-time.After(time.Millisecond * 100):
+				fmt.Printf("Timeout occured %d\n", i)
+				i++
+			}
+		}
+	}()
+
+	var input string
+	fmt.Scanln(&input)
+
+}
